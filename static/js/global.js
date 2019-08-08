13b32acc50704fde4282c79b8d9e83d63068a3bb
Ben Border <ben@bunnbuns.net>
2019-08-08 12:08:33 -0400

    changed the ios fallback

--- static/js/global.js
+++ static/js/global.js
@@ -4,10 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 //--//
 var intro = document.querySelector('.banner');
+var body = document.querySelector('body');
 var introPlayer = document.querySelector('.banner__video');
 
 var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
 if (iOS) {
-  intro.style.backgroundImage = 'url("' + introPlayer.poster + '")';
+  body.style.backgroundImage = 'url("' + introPlayer.poster + '")';
   introPlayer.style.display = 'none';
 }
